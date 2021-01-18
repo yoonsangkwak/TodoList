d63b9fd29cf5249e2faebb0172f249d030ba3923
yoonsangkwak <yunsang08@naver.com>
2021-01-18 21:17:14 +0900

    Firebase : signup login logout

--- TodoList/app/src/main/java/site/yoonsang/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/site/yoonsang/todolist/MainActivity.kt
@@ -1,21 +1,31 @@
 package site.yoonsang.todolist
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Paint
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
 import androidx.activity.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import site.yoonsang.todolist.databinding.ActivityMainBinding
 import site.yoonsang.todolist.databinding.ItemTodoBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    val RC_SIGN_IN = 1000
 
     private lateinit var binding: ActivityMainBinding
 
@@ -26,6 +36,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = Firebase.auth
+        // 로그인 안돼있으면
+        if (auth.currentUser == null) {
+            login()
+        }
+
 
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -50,6 +67,58 @@ class MainActivity : AppCompatActivity() {
         viewModel.todoLiveData.observe(this, Observer {
             (binding.recyclerView.adapter as TodoAdapter).setData(it)
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = auth.currentUser
+            } else {
+                // 로그인 실패
+                finish()
+            }
+        }
+    }
+
+    fun login() {
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                login()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.action_log_out -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
@@ -108,7 +177,7 @@ class TodoAdapter(
     }
 }
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     val todoLiveData = MutableLiveData<List<Todo>>()
 
