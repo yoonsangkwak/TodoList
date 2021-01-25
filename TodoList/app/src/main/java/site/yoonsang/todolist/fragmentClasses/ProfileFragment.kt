3f8d85616117bc50e98d0ba32096d8dcdbcc7cf7
yoonsangkwak <yunsang08@naver.com>
2021-01-25 21:29:35 +0900

    profile image setting

--- TodoList/app/src/main/java/site/yoonsang/todolist/fragmentClasses/ProfileFragment.kt
+++ TodoList/app/src/main/java/site/yoonsang/todolist/fragmentClasses/ProfileFragment.kt
@@ -1,18 +1,19 @@
 package site.yoonsang.todolist.fragmentClasses
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import site.yoonsang.todolist.R
 import site.yoonsang.todolist.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var mBinding: FragmentProfileBinding? = null
+    private val GALLERY = 200
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +22,33 @@ class ProfileFragment : Fragment() {
 
         val binding = FragmentProfileBinding.inflate(inflater, container, false)
         mBinding = binding
+
+        mBinding?.profileImageSettingBtn?.setOnClickListener {
+            loadImage()
+        }
+
         return mBinding?.root
     }
 
     override fun onDestroyView() {
         mBinding = null
         super.onDestroyView()
+    }
+
+    private fun loadImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(intent, GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY) {
+            if (resultCode == RESULT_OK) {
+                val dataUri = data?.data
+                mBinding?.profileImage?.setImageURI(dataUri)
+            }
+        }
     }
 }
